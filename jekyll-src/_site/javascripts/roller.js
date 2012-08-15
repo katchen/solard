410a2de68b61edbf7dccc2972603e02103130254
Victoria Flores <victoriaflrs@gmail.com>
2012-08-14 21:56:10 -0700

    adding a bunch of images i don't like

--- jekyll-src/_site/javascripts/roller.js
+++ jekyll-src/_site/javascripts/roller.js
@@ -1,19 +1,23 @@
-var menu = document.getElementById('menu');
-var docked;
-var init = menu.offsetTop;
+ function show_menu(id) {
+  document.getElementById(id + '_white').style.display = "block";
+  document.getElementById(id + '_left').style.display = "block";
+  //document.getElementById(id + '_right').style.display = "block";
+ // document.getElementById(id + '_line').style.display = "block";
+  //document.getElementById('menu_' + id).style.color = "#444";
+ // document.getElementById(id + '_white').style.zIndex = "1";
+  document.getElementById(id + '_left').style.zIndex = "1";
+  //document.getElementById(id + '_right').style.zIndex = "1";
+  //document.getElementById(id + '_line').style.zIndex = "1";
+}
 
-window.onscroll = function () {
-  if (!docked && (menu.offsetTop - scrollTop() < 0)) {
-    menu.style.top = 0;
-    menu.style.position = 'fixed';
-    docked = true;
-  } else if (docked && scrollTop() <= init) {
-    menu.style.position = 'absolute';
-    menu.style.top = init + 'px';
-    docked = false;
-  }
-};
-
-function scrollTop() {
-  return document.body.scrollTop || document.documentElement.scrollTop;
+function hide_menu(id) {
+  document.getElementById(id + '_white').style.display = "none";
+  document.getElementById(id + '_left').style.display = "none";
+  //document.getElementById(id + '_right').style.display = "none";
+  document.getElementById(id + '_line').style.display = "none";
+  document.getElementById('menu_' + id).style.color = "#ff7f00";
+  document.getElementById(id + '_white').style.zIndex = "0";
+  document.getElementById(id + '_left').style.zIndex = "0";
+  //document.getElementById(id + '_right').style.zIndex = "0";
+  document.getElementById(id + '_line').style.zIndex = "0";
 }
